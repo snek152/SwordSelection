f3ae7ce389d7a31dcf74812db8d21dffdbedc064
SneK152 <kakanisnehil@gmail.com>
2021-07-03 10:51:12 -0700

    Added the stone tiers of the sickles, longsword, and broadsword, as well as model and texture files and recipes
    Revamped version system in build.gradle, made it more efficient for getting the mod version

--- src/main/java/com/snek152/swordselection/item/ModItems.java
+++ src/main/java/com/snek152/swordselection/item/ModItems.java
@@ -20,7 +20,7 @@ public class ModItems {
                             new Item.Properties()
                                     .defaultMaxDamage(210)
                                     .group(ItemGroup.COMBAT)
-                                    .addToolType(ToolType.PICKAXE,0)));
+                                    .addToolType(ToolType.HOE,0)));
 
     // Iron broadsword
     public static final RegistryObject<Item> IRON_BROADSWORD =
@@ -38,6 +38,29 @@ public class ModItems {
                                     .defaultMaxDamage(250)
                                     .group(ItemGroup.COMBAT)));
 
+    // Stone sickles
+    public static final RegistryObject<Item> STONE_SICKLES =
+            Registration.ITEMS.register("stone_sickles",
+                    () -> new SwordItem(ItemTier.STONE, 0,-1.2f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(91)
+                                    .group(ItemGroup.COMBAT)
+                                    .addToolType(ToolType.HOE,0)));
 
+    // Stone broadsword
+    public static final RegistryObject<Item> STONE_BROADSWORD =
+            Registration.ITEMS.register("stone_broadsword",
+                    () -> new BroadSword(ItemTier.STONE, 5,-3.4f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(142)
+                                    .group(ItemGroup.COMBAT)));
+
+    // Stone longsword
+    public static final RegistryObject<Item> STONE_LONGSWORD =
+            Registration.ITEMS.register("stone_longsword",
+                    () -> new Longsword(ItemTier.STONE, 4,-2.4f,
+                            new Item.Properties()
+                                    .defaultMaxDamage(131)
+                                    .group(ItemGroup.COMBAT)));
     public static void register() {}
 }
