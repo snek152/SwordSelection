4f786817093014e817362215b989388769f3f283
SneK152 <kakanisnehil@gmail.com>
2021-07-12 21:56:26 -0700

    Added new specialities for the broadsword and longsword

--- src/main/java/com/snek152/swordselection/item/Broadsword.java
+++ src/main/java/com/snek152/swordselection/item/Broadsword.java
@@ -1,10 +1,20 @@
 package com.snek152.swordselection.item;
 
 import com.snek152.swordselection.util.Config;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.world.World;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.world.NoteBlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class Broadsword extends SwordItem {
 
@@ -22,6 +32,17 @@ public class Broadsword extends SwordItem {
 
         target.setDeltaMovement(look.x*knockback,look.y*knockback,look.z*knockback);
         return true;
+
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity playerIn, int itemSlot, boolean isSelected) {
+        if (playerIn.getType() == EntityType.PLAYER) {
+            if (stack.getItem().getClass() == Broadsword.class && isSelected) {
+                ((PlayerEntity) playerIn).addEffect(new EffectInstance(Effects.MOVEMENT_SLOWDOWN,0,0,false,false,false));
+            }
+
+        }
 
     }
 }
--- src/main/java/com/snek152/swordselection/item/Longsword.java
+++ src/main/java/com/snek152/swordselection/item/Longsword.java
@@ -1,15 +1,21 @@
 package com.snek152.swordselection.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.INPC;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
 
 import java.util.List;
+import java.util.Random;
 
 public class Longsword extends SwordItem {
     public Longsword(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builderIn) {
@@ -30,6 +36,25 @@ public class Longsword extends SwordItem {
         }
 
         return true;
+
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World worldIn, Entity playerIn, int itemSlot, boolean isSelected) {
+        if (playerIn.getType() == EntityType.PLAYER) {
+            if (stack.getItem().getClass() == Longsword.class && isSelected && Math.random() <= 0.1) {
+                AxisAlignedBB axis = playerIn.getBoundingBox().expandTowards(1,0,1).expandTowards(-1,0,1).expandTowards(1,0,-1).expandTowards(-1,0,-1);
+                List<Entity> entities = playerIn.getCommandSenderWorld().getEntities(playerIn,axis);
+
+                for (Entity e: entities) {
+                    if (e instanceof LivingEntity && !(e instanceof INPC)) {
+
+                        e.hurt(DamageSource.playerAttack((PlayerEntity) playerIn),((SwordItem)stack.getItem()).getDamage());
+                    }
+                }
+            }
+
+        }
 
     }
 }
